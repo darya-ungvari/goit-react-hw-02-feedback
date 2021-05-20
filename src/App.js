57106e9f61f3b3e2d51b42f84e9ece3e6628c887
darya-ungvari <63539808+darya-ungvari@users.noreply.github.com>
2021-05-20 10:32:21 +0300

    add formatting

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import FeedbackCounter from './Components/FeedbackCounter/FeedbackCounter';
 import Section from './Components/Section/Section';
 
 class App extends React.Component {
-
   state = {
     good: 0,
     neutral: 0,
@@ -15,40 +14,42 @@ class App extends React.Component {
 
   handleGoodIncrement = () => {
     this.setState({
-        good: this.state.good + 1,
+      good: this.state.good + 1,
     });
-}
+  };
 
   handleBadIncrement = () => {
     this.setState({
       bad: this.state.bad + 1,
     });
-  }
+  };
 
   handleNeutralIncrement = () => {
     this.setState({
-        neutral: this.state.neutral + 1,
+      neutral: this.state.neutral + 1,
     });
+  };
+
+  render() {
+    return (
+      <Section title="Please leave feedback">
+        <ul>
+          <ControlFeedback name="Good" myClick={this.handleGoodIncrement} />
+          <ControlFeedback
+            name="Neutral"
+            myClick={this.handleNeutralIncrement}
+          />
+          <ControlFeedback name="Bad" myClick={this.handleBadIncrement} />
+        </ul>
+
+        <FeedbackCounter
+          good={this.state.good}
+          bad={this.state.bad}
+          neutral={this.state.neutral}
+        />
+      </Section>
+    );
   }
-
- render(){
-  return (
-    <Section title="Please leave feedback">
-      <ul>
-        <ControlFeedback name='Good' myClick={this.handleGoodIncrement}/>
-        <ControlFeedback name='Neutral' myClick={this.handleNeutralIncrement}/>
-        <ControlFeedback name='Bad' myClick={this.handleBadIncrement}/>
-      </ul>
-
-       <FeedbackCounter 
-      good={this.state.good}
-      bad={this.state.bad}
-      neutral={this.state.neutral}
-      />
-    </Section>
-  );
- }
-  
 }
 
 export default App;
--- src/Components/ControlFeedback/ControlFeedback.jsx
+++ src/Components/ControlFeedback/ControlFeedback.jsx
@@ -4,10 +4,10 @@ import PropTypes from 'prop-types'
 import s from './ControlFeedback.module.css'
 
 const ControlFeedback = ({ myClick, name }) => {
-    return(       
-            <li className={s.li}>
-                <button onClick={myClick}>{name}</button>
-            </li>
+    return (
+        <li className={s.li}>
+            <button onClick={myClick}>{name}</button>
+        </li>
     )
 }
 
